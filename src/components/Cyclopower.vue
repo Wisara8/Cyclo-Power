bc950c2dce421c3a03fc4a52f287020f4143bebd
leor8 <leo442183205@gmail.com>
2018-12-22 11:23:16 -0800

    Adding two sections and can be rendered dynamically now

--- src/components/Cyclopower.vue
+++ src/components/Cyclopower.vue
@@ -15,8 +15,8 @@
         <p class="user_text"> I am a placeholderI am a placeholderI am a placeholderI am a placeholderI am a placeholder </p>
 
         <ul class="nav_list">
-          <li class="nav_item"> Item 1 </li>
-          <li class="nav_item"> Item 2 </li>
+          <li class="nav_item" v-on:click="activity"> Item 1 </li>
+          <li class="nav_item" v-on:click="second"> Item 2 </li>
           <li class="nav_item"> Item 3 </li>
           <li class="nav_item"> Item 4 </li>
         </ul>
@@ -24,24 +24,34 @@
     </nav>
 
     <content class="main_content">
-      <section class="map_section">
-        <header class="section_header">
-          <h2 class="section_title"> User Activities</h2>
-          <div class="shorter_border"></div>
-        </header>
-
-        <div class="map">
-          <h3> MAP </h3>
-        </div>
-      </section>
+      <component v-bind:is="component"></component>
     </content>
   </main>
 </template>
 
 <script>
+  import UserActivity from './UserActivity.vue';
+  import Placeholder from './Placeholder.vue';
 
-export default {
-}
+  export default {
+    components: {
+      'user_activity': UserActivity,
+      'placeholder': Placeholder
+    },
+    data() {
+      return {
+        component: 'user_activity'
+      }
+    },
+    methods: {
+      activity: function(event) {
+        this.component = 'user_activity';
+      },
+      second: function(event) {
+        this.component = 'placeholder';
+      }
+    }
+  }
 </script>
 
 
---
+++ src/components/UserActivity.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="map_section">
+    <header class="section_header">
+      <h2 class="section_title"> User Activities</h2>
+      <div class="shorter_border"></div>
+    </header>
+
+    <div class="map">
+      <h3> MAP </h3>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+
+</style>
