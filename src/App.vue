3bbd7e6a2f55e5faf3af5f2bb68a8705772bd0e4
leor8 <leo442183205@gmail.com>
2018-12-22 10:51:55 -0800

    Nav bar and its interactive component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cyclopower msg="Welcome to Cyclopower"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cyclopower from './components/Cyclopower.vue'
+
+require('./assets/css/main.css')
+require('./assets/css/nav.css')
+require('./assets/css/normalize.css')
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cyclopower
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Lucida Sans", Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Cyclopower.vue
@@ -0,0 +1,53 @@
+<template>
+  <main class="container">
+    <nav>
+      <div class="toggle_button">
+        <i class="fas fa-grip-lines fa-2x"></i>
+      </div>
+      <div class="nav_top_header">
+        <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="user_icon" class="user_icon">
+      </div>
+      <div class="nav_items">
+        <h1 class="user_name">
+          User Name
+        </h1>
+
+        <p class="user_text"> I am a placeholderI am a placeholderI am a placeholderI am a placeholderI am a placeholder </p>
+
+        <ul class="nav_list">
+          <li class="nav_item"> Item 1 </li>
+          <li class="nav_item"> Item 2 </li>
+          <li class="nav_item"> Item 3 </li>
+          <li class="nav_item"> Item 4 </li>
+        </ul>
+      </div>
+    </nav>
+
+    <content class="main_content">
+      <section class="map_section">
+        <header class="section_header">
+          <h2 class="section_title"> User Activities</h2>
+          <div class="shorter_border"></div>
+        </header>
+
+        <div class="map">
+          <h3> MAP </h3>
+        </div>
+      </section>
+    </content>
+  </main>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+
+</style>
